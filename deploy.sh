b04cdc582b716c49a60a67fc1b30c1c115fdccc5
Juan Ballesteros <43350432+baiest@users.noreply.github.com>
2020-10-31 15:44:38 -0500

    travis add

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run build
+npm build
 
 # navigate into the build output directory
 cd dist
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:anavictoria1994/Stay-at-home.git master:gh-pages
+git push -f git@github.com:anavictoria1994/Stay-at-home.git main:gh-pages
 
 cd -
